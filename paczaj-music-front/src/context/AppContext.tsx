4b02190999a43737aece53049d556390c296b387
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-13 00:45:33 +0100

    Add context and context types

---
+++ paczaj-music-front/src/context/AppContext.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { AppContextTypes, LoadingTypes } from '../types/contextTypes';
+
+export const AppContext = React.createContext<AppContextTypes>({
+  chosenArtistId: undefined,
+  setChosenArtistId: () => {},
+  loadingType: {
+    application: false,
+    artist: false,
+    release: false,
+  },
+  setLoadingType: () => {},
+  chosenYoutubeId: undefined,
+  setChosenYoutubeId: () => {},
+  chosenReleaseId: undefined,
+  setChosenReleaseId: () => {},
+});
+
+const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [loadingType, setLoadingType] = useState<LoadingTypes | undefined>({
+    application: false,
+    artist: false,
+    release: false,
+  });
+  const [chosenArtistId, setChosenArtistId] = useState<number | undefined>(
+    undefined
+  );
+  const [chosenYoutubeId, setChosenYoutubeId] = useState<string | undefined>(
+    undefined
+  );
+
+  const [chosenReleaseId, setChosenReleaseId] = useState<number | undefined>(
+    undefined
+  );
+  const initContextValue: AppContextTypes = {
+    chosenArtistId,
+    setChosenArtistId,
+    loadingType,
+    setLoadingType,
+    chosenYoutubeId,
+    setChosenYoutubeId,
+    chosenReleaseId,
+    setChosenReleaseId,
+  };
+  return (
+    <AppContext.Provider value={initContextValue}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
---
+++ paczaj-music-front/src/types/contextTypes.ts
@@ -0,0 +1,18 @@
+export interface LoadingTypes {
+  application: boolean | undefined;
+  artist: boolean | undefined;
+  release: boolean | undefined;
+}
+
+export interface AppContextTypes {
+  chosenArtistId: number | undefined;
+  setChosenArtistId: React.Dispatch<React.SetStateAction<number | undefined>>;
+  loadingType: LoadingTypes | undefined;
+  setLoadingType: React.Dispatch<
+    React.SetStateAction<LoadingTypes | undefined>
+  >;
+  chosenYoutubeId: string | undefined;
+  setChosenYoutubeId: React.Dispatch<React.SetStateAction<string | undefined>>;
+  chosenReleaseId: number | undefined;
+  setChosenReleaseId: React.Dispatch<React.SetStateAction<number | undefined>>;
+}
