cca6d7685760948e10ed883b97bd738a2d795a9a
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-13 03:38:50 +0100

    Add sidebar files, tests and types

---
+++ paczaj-music-front/src/components/Sidebar/SearchArtists/SearchArtists.tsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+
+interface SearchArtistsTypes {
+  placeholder?: string | undefined;
+  onInput: Function;
+}
+const SearchArtists: React.FC<SearchArtistsTypes> = ({
+  placeholder,
+  onInput,
+}) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const clearInputHandler = (e: any) => {
+    ref.current!.value = '';
+    onInput(e);
+  };
+
+  return (
+    <div className="artist-list__search">
+      <input
+        type="text"
+        ref={ref}
+        onInput={() => onInput(ref.current?.value)}
+        name="searchArtists"
+        aria-label="searchArtists"
+        placeholder={placeholder}
+        className="artist-list__search-input"
+        autoComplete="off"
+      />
+      {ref.current && ref.current!.value.length! > 0 && (
+        <i
+          role="button"
+          className="icon-cancel icon__clear-search"
+          onClick={clearInputHandler}
+        ></i>
+      )}
+    </div>
+  );
+};
+
+export default SearchArtists;
---
+++ paczaj-music-front/src/components/Sidebar/__tests__/SearchArtists.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import SearchArtists from '../SearchArtists/SearchArtists';
+import userEvent from '@testing-library/user-event';
+
+const onInput = jest.fn();
+
+describe('Tests for SearchArtists component', () => {
+  beforeEach(() => {
+    render(<SearchArtists onInput={onInput} placeholder="searchArtists" />);
+  });
+  it('should be rendered proper HTML structure', () => {
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue('');
+    expect(input).toHaveAttribute('type', 'text');
+  });
+
+  it('should be fire onInput  function and proper value', () => {
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'HELlO');
+    expect(onInput).toBeCalledTimes(5);
+    expect(input).toHaveValue('HELlO');
+  });
+});
