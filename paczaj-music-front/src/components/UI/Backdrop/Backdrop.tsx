fb12ee53fc009a0a9538466a63533c75fe816e70
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-15 10:59:26 +0100

    Correct Backdrop component

--- paczaj-music-front/src/components/UI/Backdrop/Backdrop.tsx
+++ paczaj-music-front/src/components/UI/Backdrop/Backdrop.tsx
@@ -1,17 +1,20 @@
 import React, { useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
+import { classNameModifiers } from '../../../uitls/utils';
 
 interface BackdropTypes {
   onClick: React.MouseEventHandler;
   trigger?: boolean | undefined;
-  type: 'application' | 'artist' | 'youtube' | undefined;
+  backdrop_prefix?: string;
+  backdrop_modifier?: string;
   withIconLoader: boolean;
   nodeRef: any;
 }
 const Backdrop: React.FC<Partial<BackdropTypes>> = ({
   onClick,
   trigger,
-  type,
+  backdrop_prefix,
+  backdrop_modifier,
   withIconLoader,
 }) => {
   const nodeRef = useRef(null);
@@ -26,12 +29,20 @@ const Backdrop: React.FC<Partial<BackdropTypes>> = ({
       appear
     >
       <div
+        role="status"
         ref={nodeRef}
         onClick={onClick}
-        className={`backdrop backdrop--${type}`}
+        className={classNameModifiers(
+          backdrop_prefix,
+          backdrop_modifier,
+          'backdrop'
+        )}
       >
         {withIconLoader && (
-          <i className="backdrop__icon icon-spinner2 icon-rotate"></i>
+          <i
+            className="backdrop__icon icon-spinner2 icon-rotate"
+            role="spinbutton"
+          ></i>
         )}
       </div>
     </CSSTransition>
