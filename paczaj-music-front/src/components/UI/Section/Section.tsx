54322e08a05808e690ba7a4d002f8fdf5622dcb2
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-18 07:50:53 +0100

    Add Section component with tests

---
+++ paczaj-music-front/src/components/UI/Section/Section.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Heading from '../Heading/Heading';
+import { classNameModifiers } from '../../../uitls/utils';
+import Divider from '../Divider/Divider';
+
+interface SectionTypes {
+  section_type: string;
+  section_title: string;
+  section_modifier?: string;
+  children: React.ReactNode;
+}
+
+const Section: React.FC<SectionTypes> = ({
+  section_title,
+  section_type,
+  section_modifier,
+  children,
+}) => {
+  return (
+    <section
+      role="presentation"
+      className={classNameModifiers(section_type, section_modifier, 'section')}
+    >
+      <Heading
+        title={section_title}
+        heading_prefix={section_type}
+        heading_modifier={section_modifier}
+      />
+      r
+      <Divider
+        divider_prefix={section_type}
+        divider_modifier={section_modifier}
+      />
+      {children}
+    </section>
+  );
+};
+
+export default Section;
---
+++ paczaj-music-front/src/components/UI/__test__/Section.spec.tsx
@@ -0,0 +1,53 @@
+import { getByRole, render, screen } from '@testing-library/react';
+import Section from '../Section/Section';
+
+describe('Tests for Section component', () => {
+  it('should Section component in the document with all elements', () => {
+    render(
+      <Section section_type="test" section_title="Test">
+        <p>Child</p>
+      </Section>
+    );
+    const section = screen.getByRole('presentation');
+    const heading = screen.getByRole('heading', { level: 2 });
+    const divider = screen.getByRole('separator');
+    const child = screen.getByText('Child');
+
+    expect(section).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+    expect(divider).toBeInTheDocument();
+    expect(child).toBeInTheDocument();
+  });
+
+  it('should all elements to have proper classes when section_type prop is given', () => {
+    render(
+      <Section section_type="test" section_title="Test">
+        <p>Test</p>
+      </Section>
+    );
+    const section = screen.getByRole('presentation');
+    const heading = screen.getByRole('heading', { level: 2 });
+    const divider = screen.getByRole('separator');
+    expect(section).toHaveClass('test__section');
+    expect(heading).toHaveClass('test__heading');
+    expect(divider).toHaveClass('test__divider');
+  });
+
+  it('should all elements to have proper classes when section_modifier prop is given', () => {
+    render(
+      <Section
+        section_modifier="modifier"
+        section_title="Test"
+        section_type="test"
+      >
+        <p>Test</p>
+      </Section>
+    );
+    const section = screen.getByRole('presentation');
+    const heading = screen.getByRole('heading', { level: 2 });
+    const divider = screen.getByRole('separator');
+    expect(section).toHaveClass('test__section--modifier');
+    expect(heading).toHaveClass('test__heading--modifier');
+    expect(divider).toHaveClass('test__divider--modifier');
+  });
+});
