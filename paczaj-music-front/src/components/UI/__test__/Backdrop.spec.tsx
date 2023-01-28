0a6746148f2ea6736db18326cad87717c01ca7f5
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-28 03:09:22 +0100

    Change folder structure

---
+++ paczaj-music-front/src/components/UI/__test__/Backdrop.spec.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Backdrop from '../Backdrop/Backdrop';
+
+const onClick = jest.fn();
+
+describe('Tests for Backdrop components', () => {
+  it('should be Backdrop in the document', () => {
+    render(<Backdrop trigger={true} />);
+    const backdrop = screen.getByRole('status');
+    expect(backdrop).toBeInTheDocument();
+    expect(backdrop).toHaveClass('backdrop');
+  });
+
+  it('should be proper class when backdrop_prefix prop given', () => {
+    render(<Backdrop trigger={true} backdrop_prefix="test" />);
+    const backdrop = screen.getByRole('status');
+    expect(backdrop).toHaveClass('test__backdrop');
+  });
+
+  it('should be proper class when backdrop_modifier prop given', () => {
+    render(<Backdrop trigger={true} backdrop_modifier="test" />);
+    const backdrop = screen.getByRole('status');
+    expect(backdrop).toHaveClass('backdrop--test');
+  });
+
+  it('should be proper icon when withIconLoader prop is given', () => {
+    render(<Backdrop trigger={true} backdrop_modifier="test" withIconLoader />);
+    const backdrop = screen.getByRole('status');
+    const icon = screen.getByRole('spinbutton');
+    expect(backdrop).toContainElement(icon);
+  });
+
+  it('should be given function fired once when clicked and onClich prop is given', () => {
+    render(
+      <Backdrop trigger={true} backdrop_modifier="test" onClick={onClick} />
+    );
+    const backdrop = screen.getByRole('status');
+    userEvent.click(backdrop);
+    expect(onClick).toBeCalledTimes(1);
+  });
+});
