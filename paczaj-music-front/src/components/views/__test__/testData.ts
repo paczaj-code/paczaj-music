009d4b6d52a936d987dca885f4d95bd1a662e22c
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-28 04:41:33 +0100

    Add more test data

--- paczaj-music-front/src/components/views/__test__/testData.ts
+++ paczaj-music-front/src/components/views/__test__/testData.ts
@@ -3,12 +3,53 @@ After moonlighting in several underground Detroit bands as a drummer, White foun
 White is a board member of the Library of Congress' National Recording Preservation Foundation. His record label and studio Third Man Records releases vinyl recordings of his own work as well as that of other artists and local school children. His second studio album, Lazaretto (2014), holds the record for most first-week vinyl sales since 1991. White has an extensive collection of guitars and other instruments and has a preference for vintage items that often have connections to famous blues artists. He is a vocal advocate for analog technology and recording techniques.`;
 
 export const testData = {
-  country: 'Poland',
-  country_code: 'pl',
-  begin_date_year: 1999,
-  end_date_year: 2020,
-  tags: ['punk', 'indie'],
-  artist_description: artistDesc,
-  wikipedia_link: 'some_link',
-  artist_name: 'Artist',
+  artist_data: {
+    country: 'Poland',
+    country_code: 'pl',
+    begin_date_year: 1999,
+    end_date_year: 2020,
+    tags: ['punk', 'indie'],
+    artist_description: artistDesc,
+    wikipedia_link: 'some_link',
+    artist_name: 'Artist',
+  },
+  releases: {
+    studio_albums: [
+      {
+        first_release_date: 2003,
+        front_small:
+          'https://archive.org/download/mbid-53333ba5-b045-3123-bdd6-70e23a92fbb8/mbid-53333ba5-b045-3123-bdd6-70e23a92fbb8-2981703601_thumb250.jpg',
+        id: 148061,
+        name: 'Get Born',
+      },
+      {
+        first_release_date: 2009,
+        front_small:
+          'https://archive.org/download/mbid-537ce572-5c29-45d0-b3c2-d54acdd9dba4/mbid-537ce572-5c29-45d0-b3c2-d54acdd9dba4-32299499242_thumb250.jpg',
+        id: 865250,
+        name: 'Shaka Rock',
+      },
+    ],
+    extended_plays: [
+      {
+        first_release_date: 2004,
+        front_small: null,
+        id: 402612,
+        name: 'Cold Hard Bitch',
+      },
+    ],
+    compilations: [
+      {
+        first_release_date: 2018,
+        front_small:
+          'https://archive.org/download/mbid-6c60f0b4-28ae-4679-8547-3f9ba6b5c305/mbid-6c60f0b4-28ae-4679-8547-3f9ba6b5c305-22267026325_thumb250.jpg',
+        id: 2109442,
+        name: 'Are You Gonna Be My Girl',
+      },
+    ],
+  },
+  youtube_movies: [
+    { title: 'Jet - Are You Gonna Be My Girl', youtube_id: 'tuK6n2Lkza0' },
+    { title: "Jet - Look what you've done", youtube_id: 'XD1cxSE25ck' },
+  ],
 };
