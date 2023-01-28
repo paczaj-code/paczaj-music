4fe267bb9987f73fbeed56a2c2a4bd34c9471810
paczaj-code <marcinpaczkowski@icloud.com>
2023-01-28 04:42:35 +0100

    Reactor code

--- paczaj-music-front/src/components/views/ArtistView/ArtistView.tsx
+++ paczaj-music-front/src/components/views/ArtistView/ArtistView.tsx
@@ -1,23 +1,24 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
+import useAxios from '../../../hooks/useAxios';
 import { CSSTransition } from 'react-transition-group';
 import { AppContext } from '../../../context/AppContext';
 import { ArtistDetailTypes } from '../../../types/artistDetailTypes';
+import { ReleaseTypes } from '../../../types/releaseTypes';
 import { YoutubeTypes } from '../../../types/youtubeTypes';
-import useAxios from '../../../hooks/useAxios';
-// import Section from '../../UI/Section/Section';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import SectionArtistDetails from './SectionArtistDetails/SectionArtistDetails';
-import SectionYoutube from './SectionYoutube/SectionYoutube';
+import SectionYoutube from './SectionYoutube';
+import SectionReleases from './SectionReleases';
 
 interface ArtistViewTypes {
   artist_data?: ArtistDetailTypes;
   youtube_movies?: YoutubeTypes[];
-  // releases: {
-  //   studio_albums: ReleaseTypes[];
-  //   extended_plays: ReleaseTypes[];
-  //   compilations: ReleaseTypes[];
-  // };
+  releases?: {
+    studio_albums: ReleaseTypes[];
+    extended_plays: ReleaseTypes[];
+    compilations: ReleaseTypes[];
+  };
 }
 
 const ArtistView: React.FC<ArtistViewTypes> = () => {
@@ -25,7 +26,7 @@ const ArtistView: React.FC<ArtistViewTypes> = () => {
     ArtistViewTypes | undefined
   >(undefined);
 
-  const { chosenArtistId, loadingType, setLoadingType } =
+  const { chosenArtistId, loadingType, setLoadingType, chosenReleaseId } =
     useContext(AppContext);
 
   const { isLoading } = useAxios(
@@ -46,6 +47,8 @@ const ArtistView: React.FC<ArtistViewTypes> = () => {
   }, [isLoading, setLoadingType, loadingType?.artist]);
 
   const nodeRef = useRef(null);
+  console.log(chosenReleaseId);
+
   return (
     <>
       <Helmet>
@@ -74,6 +77,24 @@ const ArtistView: React.FC<ArtistViewTypes> = () => {
             end_date_year={artistDetails?.artist_data?.end_date_year}
           ></SectionArtistDetails>
           <SectionYoutube youtubeMovies={artistDetails?.youtube_movies} />
+          {artistDetails?.releases!.studio_albums.length && (
+            <SectionReleases
+              releases={artistDetails?.releases.studio_albums}
+              title="Albumy studyjne"
+            />
+          )}
+          {artistDetails?.releases!.extended_plays.length && (
+            <SectionReleases
+              releases={artistDetails?.releases.extended_plays}
+              title="EP's"
+            />
+          )}
+          {artistDetails?.releases!.compilations.length && (
+            <SectionReleases
+              releases={artistDetails?.releases.compilations}
+              title="Kompilacje"
+            />
+          )}
         </>
       </CSSTransition>
     </>
